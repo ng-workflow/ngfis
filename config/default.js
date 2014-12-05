ed7a91436459787eb52c35cc89f41019edd13aa7
TZ <atian25@qq.com>
2014-12-05 13:14:08 +0800

    add roadmap path
    Signed-off-by: TZ <atian25@qq.com>

--- config/default.js
+++ config/default.js
@@ -14,7 +14,7 @@ module.exports = {
     },
     prepackager: [],
     postpackager: [],
-    deploy: []
+    //deploy: []
   },
   urlPrefix: '',
   framework: {
@@ -23,6 +23,66 @@ module.exports = {
     comboPattern: '/co??%s'
   },
   roadmap: {
-    path: []
+    path: [
+      {
+        reg: /.*\.tpl\.html$/,
+        release: false
+      },
+      {
+        reg : '**.md',
+        isHtmlLike : true,
+        release : false
+      },
+      {
+        reg : /\.inline\.\w+$/i,
+        release : false
+      },
+      {
+        reg : /^\/components\/(.*)\.(styl|css)$/i,
+        id : '${name}/${version}/lib/$1.css',
+        isMod : true,
+        useSprite : true,
+        useHash : false,
+        url : '${urlPrefix}/${name}/${version}/lib/$1.$2',
+        release : '/public/${name}/${version}/lib/$1.$2'
+      },
+      {
+        reg : /^\/components\/(.*\.js)$/i,
+        id : '${name}/${version}/lib/$1',
+        isMod : true,
+        isComponent : true,
+        useHash : false,
+        url : '${urlPrefix}/${name}/${version}/lib/$1',
+        release : '/public/${name}/${version}/lib/$1'
+      },
+      {
+        reg : /^\/components\/(.*)$/i,
+        url : '${urlPrefix}/${name}/${version}/lib/$1',
+        release : '/public/${name}/${version}/lib/$1'
+      },
+      {
+        reg : /^\/views\/(.*\.(?:html?|js))$/,
+        useCache : false,
+        isViews : true,
+        url : '${urlPrefix}/${name}/${version}/$1',
+        release : '/public/${name}/${version}/$1'
+      },
+      {
+        reg : /^\/views\/(.*)$/,
+        useSprite : true,
+        isViews : true,
+        url : '${urlPrefix}/${name}/${version}/$1',
+        release : '/public/${name}/${version}/$1'
+      },
+      {
+        reg : 'map.json',
+        release : false
+      },
+      {
+        reg : '**',
+        useHash : false,
+        useCompile : false
+      }
+    ]
   }
 };
