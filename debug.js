9a7e56b4118f2a1fd0fda00ef07b499395312318
TZ <atian25@qq.com>
2014-12-20 11:52:08 +0800

    移除root目录, 不再有src.
    Signed-off-by: TZ <atian25@qq.com>

--- debug.js
+++ debug.js
@@ -6,24 +6,25 @@ var fis = require('./index.js').fis;
 require(root + '/fis-conf.js');
 
 // debug release
-var args = process.argv.concat([
-  'release',
-  '-c',
-  '-p',
-  '-r', fis.project.getProjectPath(), //root + '/src',
-  '-d', '../dist'
-]);
+//var args = process.argv.concat([
+//  'release',
+//  '-c',
+//  '-p',
+//  '-r', fis.project.getProjectPath(), //root + '/src',
+//  '-d', '../dist'
+//]);
 
 // debug install
-//var args = process.argv.concat([
-//  'install',
-//  'zepto',
-//  'bootstrap',
-//  //'-c',
-//  '--dev',
-//  //'--save-dev',
-//  '-d', 'dist'
-//]);
+var args = process.argv.concat([
+  'install',
+  'zepto',
+  'bootstrap',
+  //'ui-router',
+  //'-c',
+  //'--dev',
+  //'--save-dev',
+  //'-d', 'dist'
+]);
 
 fis.cli.run(args);
 
--- index.js
+++ index.js
@@ -13,9 +13,9 @@ var config = require('./config/default.js');
 fis.config.merge(config);
 
 //register command plugins
-['install'].forEach(function(name){
-  fis.require._cache['command-' + name] = require('./lib/command/' + name);
-});
+//['install'].forEach(function(name){
+//  fis.require._cache['command-' + name] = require('./lib/command/' + name);
+//});
 
 //alias
 Object.defineProperty(global, fis.cli.name, {
@@ -25,29 +25,29 @@ Object.defineProperty(global, fis.cli.name, {
 });
 
 //find root
-var root = utils.getProjectDir();
-if(!root){
-  fis.log.error('fis.conf not found.');
-}else{
-  fis.project.configDir = root;
-  root = path.join(root, 'src');
-  fis.project.setProjectRoot(root);
-  if(process.cwd() !== root){
-    process.chdir(root);
-  }
-}
-console.log(process.argv)
-
-
-if(process.argv[2] == 'release'){
-  if(process.argv.indexOf('-r') === -1 || process.argv.indexOf('--root') === -1) {
-    process.argv.push('-r');
-    process.argv.push(root);
-  }
-  if(process.argv.indexOf('-f') === -1 || process.argv.indexOf('--file') === -1) {
-    process.argv.push('-f');
-    process.argv.push(path.join(fis.project.configDir, 'fis-conf.js'));
-  }
-}
+//var root = utils.getProjectDir();
+//if(!root){
+//  fis.log.error('fis.conf not found.');
+//}else{
+//  fis.project.configDir = root;
+//  root = path.join(root, 'src');
+//  fis.project.setProjectRoot(root);
+//  if(process.cwd() !== root){
+//    process.chdir(root);
+//  }
+//}
+//console.log(process.argv)
+//
+//
+//if(process.argv[2] == 'release'){
+//  if(process.argv.indexOf('-r') === -1 || process.argv.indexOf('--root') === -1) {
+//    process.argv.push('-r');
+//    process.argv.push(root);
+//  }
+//  if(process.argv.indexOf('-f') === -1 || process.argv.indexOf('--file') === -1) {
+//    process.argv.push('-f');
+//    process.argv.push(path.join(fis.project.configDir, 'fis-conf.js'));
+//  }
+//}
 
 
